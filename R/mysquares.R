22b3a18d7d509db1ea71527f6814fbda9374d641
Spandan Pandey <spandanpandey10924@gmail.com>
2022-09-14 17:13:00 -0400

    Add examples to mysquares description.

--- R/mysquares.R
+++ R/mysquares.R
@@ -1,6 +1,9 @@
 #' Squaring function
 #' @param x A vector of numbers
 #' @returns A vector of numbers squared
+#' @examples
+#' a <- seq(0,100,10)
+#' mysquares(a)
 #' @export
 
 mysquares <- function(x) x^2
